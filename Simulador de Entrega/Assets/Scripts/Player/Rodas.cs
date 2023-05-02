1c85c934c47ff16fb2a657e24e75c711c3e260a9
Kuro <iaguinhodosantos2@gmail.com>
2023-05-02 09:12:19 -0300

    Carro Bomba

--- Simulador de Entrega/Assets/Scripts/Player/Rodas.cs
+++ Simulador de Entrega/Assets/Scripts/Player/Rodas.cs
@@ -26,12 +26,21 @@ public class Rodas : MonoBehaviour
 
     public AnimationCurve coefAtrito;
 
+    public AnimationCurve fLongNorm; //força longitudinal normalizada
+    public float slipRatio;
+    public float torqueNaRoda;
+    public float torqueResultante;
+    public float veloAngularRoda;
+    public float acelAngular;
+    public float massaRoda;
+    public float inerciaRoda;
+
     Vector3 coleta;
-    Vector3 velocidadeLocal;
+    Vector3 velocidadeLocal = new Vector3(0.0001f, 0.0001f, 0.0001f)    ;
 
      void Update()
     {
-        rotRoda.transform.Rotate(Mathf.Rad2Deg * velocidadeLocal.z / raioRoda * Time.deltaTime, 0,0);
+        rotRoda.transform.Rotate(Mathf.Rad2Deg * veloAngularRoda * Time.deltaTime, 0,0);
     }
      void Start()
     {
@@ -41,7 +50,17 @@ public class Rodas : MonoBehaviour
     {
         if(Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, distanciaSusp + raioRoda))
         {
+            inerciaRoda = massaRoda * Mathf.Pow(raioRoda, 2) / 2;
+
+            torqueResultante = (Input.GetAxis("Vertical") * torqueNaRoda) + (-forcaAtritoLongitudinal * raioRoda);
+            acelAngular = torqueResultante / inerciaRoda;
+            veloAngularRoda += acelAngular * Time.fixedDeltaTime;
+            veloAngularRoda = Mathf.Clamp(veloAngularRoda, -100, 450);
+
+            slipRatio = Mathf.Clamp(((veloAngularRoda * raioRoda) - velocidadeLocal.z) / velocidadeLocal.magnitude,-1,1);
+
             coleta = hit.point;
+            //Transformando velocidade global em velocidade local
             velocidadeLocal = transform.InverseTransformDirection(rb.GetPointVelocity(hit.point));
 
             Debug.DrawLine(transform.position, hit.point);
@@ -66,9 +85,11 @@ public class Rodas : MonoBehaviour
            //forca de resistencia ao rolamento do pneu
             resRolamento = carroceria.fArrasto * 30 * Mathf.Abs(velocidadeLocal.z);
 
+            //forca de atrito lateral
             forcaAtritoLateral = (coefAtrito.Evaluate(forcasus) * forcasus) * Mathf.Sign(velocidadeLocal.x);
 
-            forcaAtritoLongitudinal = forcasus * Input.GetAxis("Vertical");
+            //forca de atrito longitudinal
+            forcaAtritoLongitudinal = coefAtrito.Evaluate(forcasus) * forcasus * fLongNorm.Evaluate(slipRatio);
 
             //forca sendo aplicada
             rb.AddForceAtPosition((forcasus * transform.up) + (forcaAtritoLateral * -transform.right) + ((forcaAtritoLongitudinal + resRolamento) * transform.forward), hit.point);
