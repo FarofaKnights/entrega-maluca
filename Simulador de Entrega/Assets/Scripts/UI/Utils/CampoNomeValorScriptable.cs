119e0e3326d5526850ff414bd8ac1c2c528503cb
Juan Vitor <juanriven10@gmail.com>
2023-11-08 19:04:07 -0300

    🤠 Esse povo animado
    Tou com pressa pra sair de casa não vai ter nenhuma descrição chique, xauu
    
    - Relatório agora possui animação
    - Atualizei os materiais da rua pra não ficar só preto

---
+++ Simulador de Entrega/Assets/Scripts/UI/Utils/CampoNomeValorScriptable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Campo Nome Valor", menuName = "Utils Malucos"), System.Serializable]
+public class CampoNomeValorScriptable : ScriptableObject {
+    public float tempoFadeIn = 0.3f;
+    public float tempoShowValor = 0.75f;
+    public float tempoJumpValor = 0.2f;
+    public float scaleMultJump = 1.25f;
+}
