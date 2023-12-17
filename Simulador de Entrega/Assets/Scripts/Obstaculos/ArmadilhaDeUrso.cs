fc55c7a7b725c554163c85f2e97001de4905f3a2
Henri Lopes <henrilopes16@gmail.com>
2023-12-17 17:04:22 -0300

    Obstáculos finalizados

--- Simulador de Entrega/Assets/Scripts/Obstaculos/ArmadilhaDeUrso.cs
+++ Simulador de Entrega/Assets/Scripts/Obstaculos/ArmadilhaDeUrso.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class ArmadilhaDeUrso : MonoBehaviour
 {
     [SerializeField] AudioSource barulho;
-    Animation animacao;
     [SerializeField] float tempo;
+    public Animator anim;
+    public bool hasAnim = true;
+
     void Action(Rigidbody rb)
     {
         rb.velocity -= rb.velocity;
         rb.constraints = RigidbodyConstraints.FreezeAll;
+        barulho.Play();
+        if(hasAnim)
+        {
+            anim.SetBool("Desativo", false);
+            anim.SetBool("Ativo", true);
+        }
         StartCoroutine(Wait(rb));
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            barulho.Play();
-            //animacao.Play();
             Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
             Action(rb);
         }
@@ -26,6 +32,15 @@ public class ArmadilhaDeUrso : MonoBehaviour
     IEnumerator Wait(Rigidbody rb)
     {
         yield return new WaitForSeconds(tempo);
+        if(hasAnim)
+        {
+            anim.SetBool("Desativo", true);
+            anim.SetBool("Ativo", false);
+        }
         rb.constraints = RigidbodyConstraints.None;
+        if(hasAnim)
+        {
+             anim.SetBool("Desativo", false);
+        }
     }
 }
